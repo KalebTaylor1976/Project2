8fd3701e1084959a095a21729aa8805c9ae6581f
Kaleb Taylor <kaleb.taylor1976@gmail.com>
2023-01-19 07:07:43 -0800

    add Index.html, add scripts.js

---
+++ scripts.js
@@ -0,0 +1,20 @@
+document.getElementById("language-suggester-form").addEventListener("submit", function(event) {
+  event.preventDefault();
+  
+  // Collect user's answers
+  var experienceLevel = document.getElementById("experience-level").value;
+  var interests = document.getElementById("interests").value;
+  var goals = document.getElementById("goals").value;
+  var platforms = document.getElementById("platforms").value;
+  var preference = document.getElementById("preference").value;
+
+  // Determine language suggestion
+  var language = "";
+  if (experienceLevel === "beginner" && interests.includes("web-development")) {
+    language = "JavaScript";
+  } else if (experienceLevel === "intermediate" && interests.includes("mobile-app-development")) {
+    language = "C#";
+  } else if (experienceLevel === "advanced" && interests.includes("machine-learning")) {
+    language = "Python";
+  } else if (preference === "yes") {
+    language = "English" }   )
